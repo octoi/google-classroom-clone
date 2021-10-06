2e206a589b43e1c557b0fce7a0c9ffe964710d79
fadhilsaheer <fadhilsaheer@gmail.com>
2021-10-06 15:11:42 +0530

    custom cards

--- app/lib/screens/home_screen.dart
+++ app/lib/screens/home_screen.dart
@@ -39,12 +39,39 @@ class HomeScreen extends StatelessWidget {
         padding: EdgeInsets.all(10.0),
         child: Column(
           children: const [
-            ClassCard(),
-            ClassCard(),
-            ClassCard(),
-            ClassCard(),
-            ClassCard(),
-            ClassCard(),
+            ClassCard(
+              image:
+                  'https://www.gstatic.com/classroom/themes/img_breakfast.jpg',
+              periodName: 'Biology',
+              teacher: 'John Doe',
+            ),
+            ClassCard(
+              image:
+                  'https://www.gstatic.com/classroom/themes/img_bookclub.jpg',
+              periodName: 'English',
+              teacher: 'Peter Parker',
+            ),
+            ClassCard(
+              image: 'https://www.gstatic.com/classroom/themes/Honors.jpg',
+              periodName: 'History',
+              teacher: 'Bruce Banner',
+            ),
+            ClassCard(
+              image:
+                  'https://www.gstatic.com/classroom/themes/img_learnlanguage.jpg',
+              periodName: 'Maths',
+              teacher: 'Harry Potter',
+            ),
+            ClassCard(
+              image: 'https://www.gstatic.com/classroom/themes/img_code.jpg',
+              periodName: 'Geography',
+              teacher: 'Tony Stark',
+            ),
+            ClassCard(
+              image: 'https://www.gstatic.com/classroom/themes/Chemistry.jpg',
+              periodName: 'Chemistry',
+              teacher: 'Thor',
+            ),
           ],
         ),
       ),
