84fc72fe0654f63b43d8f2fee21fd83e66f014c9
fadhilsaheer <fadhilsaheer@gmail.com>
2021-10-06 14:41:16 +0530

    appbar

--- app/lib/screens/home_screen.dart
+++ app/lib/screens/home_screen.dart
@@ -6,7 +6,34 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0.5,
+        leading: IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.menu),
+          color: Colors.black,
+        ),
+        title: const Text(
+          'Google Classroom',
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.normal,
+          ),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.add),
+            color: Colors.black,
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.more_vert),
+            color: Colors.black,
+          ),
+        ],
+      ),
     );
   }
 }
